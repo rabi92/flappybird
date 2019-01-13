5e4ae6fed19795b1f384b6e12b08c3a009cf3b1b
Rabi <rabi.ulislam92@gmail.com>
2019-01-14 03:37:08 +0400

    Game Manager, Main menu UI, in Game UI, Game Over UI

--- Assets/Scripts/Bird_Controller.cs
+++ Assets/Scripts/Bird_Controller.cs
@@ -11,6 +11,8 @@ public class Bird_Controller : MonoBehaviour {
 
     Rigidbody2D birdRigidbody;
 
+    [SerializeField]
+    GameManager gameManager;
 	// Use this for initialization
 	void Start () {
         birdRigidbody =  GetComponent<Rigidbody2D>();
@@ -22,14 +24,22 @@ public class Bird_Controller : MonoBehaviour {
         gameover = true;
     }
 
+    public void Jump()
+    {
+        if(!gameover && !gameManager.isPaused)
+        {
+            //birdRigidbody.velocity = Vector2.zero;
+            //birdRigidbody.AddForce(jump_force * 100f * Vector2.up);
+        }
+    }
     // Update is called once per frame
     void Update () {
 		if(!gameover)
         {
-            if(Input.GetMouseButtonDown(0))
+            if(Input.GetMouseButtonDown(0) && !gameManager.isPaused)
             {
                 birdRigidbody.velocity = Vector2.zero;
-                birdRigidbody.AddForce(jump_force * 100f * Vector2.up );
+                birdRigidbody.AddForce(jump_force * 100f * Vector2.up);
             }
         }
     }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    float game_speed;
+
+    public bool isPaused = true;
+    public bool isGameOver = false;
+    int score = 0;
+
+    [SerializeField]
+    GameObject menu, gameoverUI, pauseUI, pauseButton;
+
+    [SerializeField]
+    Text scoreField;
+
+    [SerializeField]
+    Bird_Controller bird;
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        //disable menu
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void UpdateScore()
+    {
+        score++;
+        scoreField.text = score.ToString();
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        print("Pause Button Pressed!");
+        //pause game
+        isPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void GameOver()
+    {
+        //Show Gameover UI
+        gameoverUI.SetActive(true);
+        pauseButton.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        bird.transform.position = Vector3.zero;
+        //reset bird y
+        //reset obstacles
+        //reset score
+
+        //update booleans
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
